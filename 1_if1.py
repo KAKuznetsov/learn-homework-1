be176b43424917234f68fa5bc7768a855483ef66
Kirill Kuznetsov <herdboy@yandex.ru>
2024-12-08 20:07:50 +0300

    made homework 1_if1.py

--- 1_if1.py
+++ 1_if1.py
@@ -19,7 +19,30 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    user_age_text = input("Введите свой возраст: ")
+    user_age_text_clean = user_age_text.replace(",", ".")
+    user_age_text_float = float(user_age_text_clean)
+
+    # print(user_age_text_clean)
+    # print(type(user_age_text))
+    # print(type(user_age_text_clean))
+    # print(type(user_age_text_float))
+
+    def type_of_task(age):
+        if age < 0:
+            return "Введите пожалуйста реальный возраст, больше нуля!"
+        elif age < 7:
+            return "Вам необходимо учиться в детском саду!"
+        elif age < 17:
+            return "Вам необходимо учиться в школе!"
+        elif age < 23:
+            return "Вам необходимо учиться в ВУЗе!" 
+        else:
+            return "Вам необходимо работать!"         
+
+    user_result = type_of_task(user_age_text_float)
+    print(user_result)
+
 
 if __name__ == "__main__":
     main()
